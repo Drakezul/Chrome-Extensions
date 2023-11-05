d53052e820ec42b1ef8e2c510972412cbe04bc6a
Drakezul <12485878+Drakezul@users.noreply.github.com>
2023-11-05 21:28:00 +0100

    Archive deleted/removed bookmarks

--- bookmark-dedupe-chapters/src/ts/background.ts
+++ bookmark-dedupe-chapters/src/ts/background.ts
@@ -6,38 +6,22 @@ interface ChapterBookmarkTreeNode extends chrome.bookmarks.BookmarkTreeNode {
     chapter?: number;
 }
 
-const DEFAULT_REGEX = "(\\d+\\-\\d+|\\d+\\.\\d+|\\d+)"
+const BASE_REGEX = "(\\d+\\-\\d+|\\d+\\.\\d+|\\d+)"
+const DEFAULT_REGEX = "chapter\\-" + BASE_REGEX
 
-const HOSTS_WITH_CUSTOM_REGEX = [
-    {
-        host: "reaperscans.com",
-        regex: "chapter\\-" + DEFAULT_REGEX
-    },
-    {
-        host: "www.asurascans.com",
-        regex: "chapter\\-" + DEFAULT_REGEX
-    }
-]
+// const HOSTS_WITH_CUSTOM_REGEX = [
+//     {
+//         hosts: ["reaperscans.com", "asurascans.com", "asuratoon.com", "asura.gg", "luminousscans.gg"],
+//         regex: "chapter\\-" + DEFAULT_REGEX
+//     }
+// ]
 
-function chapterDedupeChilds(node: chrome.bookmarks.BookmarkTreeNode) {
-    let nodes = node.children
-    if (nodes == undefined) {
-        console.log("No children found for ", node)
-        return
-    }
-    let folders = nodes.filter((node) => node.children !== undefined)
-    for (let folder of folders) {
-        console.log("Recursing to folder " + folder.title)
-        chapterDedupeChilds(folder)
-    }
-    let nonFolderChildren = nodes.filter((node) => node.children === undefined)
+function removeDuplicates(nonFolderChildren: chrome.bookmarks.BookmarkTreeNode[]) {
     let urlBasePathIndex: { [baseUrl: string]: ChapterBookmarkTreeNode } = {}
     for (let child of nonFolderChildren) {
         let url = new URL(child.url)
 
-        let regex = HOSTS_WITH_CUSTOM_REGEX.find((mapping) => mapping.host == url.host)?.regex || DEFAULT_REGEX
-
-        let match = url.pathname.match(regex)
+        let match = url.pathname.match(DEFAULT_REGEX)
         if (match === null) {
             console.log("No match for ", url)
             continue
@@ -45,22 +29,63 @@ function chapterDedupeChilds(node: chrome.bookmarks.BookmarkTreeNode) {
         let chapter = parseFloat(match[1].replace("-", "."))
 
         let basePath = url.pathname.substring(0, match.index)
+        console.debug("Base path ", basePath)
 
         if (basePath in urlBasePathIndex) {
             if (urlBasePathIndex[basePath].chapter > chapter) {
-                chrome.bookmarks.remove(child.id)
-                console.log("Removed ", child)
+                chrome.bookmarks.move(child.id, { parentId: todaysArchiveN.id })
+                console.log("Moved existing ", child, "to archive ", todaysArchiveN.title)
             } else {
-                chrome.bookmarks.remove(urlBasePathIndex[basePath].id)
-                console.log("Removed ", urlBasePathIndex[basePath])
+                chrome.bookmarks.move(urlBasePathIndex[basePath].id, { parentId: todaysArchiveN.id })
+                console.log("Removed newly created node ", urlBasePathIndex[basePath])
             }
         } else {
             urlBasePathIndex[basePath] = child
             urlBasePathIndex[basePath].chapter = chapter
         }
-        // let m = url.pathname.match("(?:.*?)(\\d+\\-\\d+|\\d+\\.\\d+|\\d+)[a-zA-Z\\-]*\\/?\\??.*$")
     }
 }
+
+function dedupeExactMatch(node: chrome.bookmarks.BookmarkTreeNode) {
+    let childs = node.children || []
+    let urls = new Set()
+    for (let child of childs) {
+        if (child.children) {
+            dedupeExactMatch(child)
+        } else {
+            if (urls.has(child.url)) {
+                chrome.bookmarks.remove(child.id)
+            } else {
+                urls.add(child.url)
+            }
+        }
+    }
+}
+
+function chapterDedupeChilds(node: chrome.bookmarks.BookmarkTreeNode) {
+    let nodes = node.children
+    if (nodes == undefined) {
+        console.log("No children found for ", node)
+        return
+    }
+    if (node.title.startsWith("_")) {
+        console.log("Aborting dedupe and recursion for " + node.title)
+        return
+    }
+    if (node.title === archiveTitle || node.parentId == archiveN.id) {
+        console.log("Found archive, going for exactMatch dedupe")
+        return dedupeExactMatch(node)
+    }
+    let folders = nodes.filter((node) => node.children !== undefined)
+    for (let folder of folders) {
+        console.log("Recursing to folder " + folder.title)
+        chapterDedupeChilds(folder)
+    }
+    let nonFolderChildren = nodes.filter((node) => node.children === undefined)
+    removeDuplicates(nonFolderChildren)
+    // let m = url.pathname.match("(?:.*?)(\\d+\\-\\d+|\\d+\\.\\d+|\\d+)[a-zA-Z\\-]*\\/?\\??.*$")
+}
+
 
 
 function dedupeSiblings(_: string, objectWithParentId: { parentId?: string }) {
@@ -78,15 +103,38 @@ function dedupeSiblings(_: string, objectWithParentId: { parentId?: string }) {
 function dedupeSiblingsCreate(_: string, bookmark: chrome.bookmarks.BookmarkTreeNode) {
     console.log("dedupeSiblingsCreate")
     dedupeSiblings(_, bookmark)
+    console.log("Done with dedupeSiblingsCreate")
 }
 
 function dedupeSiblingsMove(_: string, moveInfo: chrome.bookmarks.BookmarkMoveInfo) {
     console.log("dedupeSiblingsMove")
     dedupeSiblings(_, moveInfo)
+    console.log("Done with dedupeSiblingsMove")
 }
 
 chrome.bookmarks.onCreated.addListener(dedupeSiblingsCreate)
 chrome.bookmarks.onMoved.addListener(dedupeSiblingsMove)
 
+const archiveTitle = "Archive"
+let archiveN: chrome.bookmarks.BookmarkTreeNode = undefined;
+let todaysArchiveN: chrome.bookmarks.BookmarkTreeNode = undefined;
 // initial execution
-chrome.bookmarks.getTree((rootNodes => chapterDedupeChilds(getUserBookmarksFromBar(rootNodes))));
+chrome.bookmarks.getTree((rootNodes) => {
+    let userBookmarks = getUserBookmarksFromBar(rootNodes)
+    archiveN = userBookmarks.children.find((node) => (node.title === archiveTitle))
+    if (!archiveN) {
+        chrome.bookmarks.create({
+            parentId: userBookmarks.id,
+            title: archiveTitle
+        }, (newNode) => (archiveN = newNode))
+    }
+    let todaysDate = new Date().toISOString().split("T")[0];
+    todaysArchiveN = archiveN.children.find((node) => node.title === todaysDate)
+    if (!todaysArchiveN) {
+        chrome.bookmarks.create({
+            parentId: archiveN.id,
+            title: todaysDate
+        }, (newNode) => (todaysArchiveN = newNode))
+    }
+    chapterDedupeChilds(userBookmarks)
+});
